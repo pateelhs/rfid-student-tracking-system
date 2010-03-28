f28c67623a756b4cd467b2e321cd2d62bc4b42c7
usmanzafar786 <usmanzafar786@1c67d6c6-17fa-11df-b4e2-7345042c47b0>
2010-03-28 18:19:53 +0000

    2D Map of FAST-NU

---
+++ Fyp_Proj/Fyp_Proj/Map.Designer.cs
@@ -0,0 +1,47 @@
+namespace Fyp_Proj
+{
+    partial class Map
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // DrawShapesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(1F, 1F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 188);
+            this.Name = "DrawShapesForm";
+            this.Text = "Drawing Shapes";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawShapesForm_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
